84fe77868ac0db50578bee3f5b842319cff66440
sani.usnain <sani.usnain@onescreensolutions.com>
2024-06-04 10:57:38 +0500

    Added POST route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,12 @@ app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.get('/room', (req, res) => res.send("I am in the room"))
 
+app.post('/room', (req,res)=>{
+    const {name, email} = req.body;
+
+    res.send(`I am ${name} and you can contact me on ${email}`)
+})
+
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
