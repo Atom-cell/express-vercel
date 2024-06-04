2f2f903baac1000f146b130988bf6ecef6d9a72f
sani.usnain <sani.usnain@onescreensolutions.com>
2024-06-04 11:05:47 +0500

    post method corrected

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(express.json());
 
-app.get('/room', (req, res) => res.send("I am in the room"))
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
 app.post('/room', (req,res)=>{
     const {name, email} = req.body;
 
     res.send(`I am ${name} and you can contact me on ${email}`)
 })
+
+app.get('/room', (req, res) => res.send("I am in the room"))
+
+
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
